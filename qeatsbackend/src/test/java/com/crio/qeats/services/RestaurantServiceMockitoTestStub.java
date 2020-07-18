2c5daa9fca6322f7a9e52f59fe1f185d3d1a687a
h4hari1997 <h4hari2015@gmail.com>
2020-07-19 04:41:06 +0530

    ME_QEATS_V2_MODULE_MOCKITO

--- qeatsbackend/src/test/java/com/crio/qeats/services/RestaurantServiceMockitoTestStub.java
+++ qeatsbackend/src/test/java/com/crio/qeats/services/RestaurantServiceMockitoTestStub.java
@@ -72,7 +72,7 @@ public class RestaurantServiceMockitoTestStub {
     // Notes - You can create additional mocks, setup the same and try out.
 
 
-     when(restaurantRepositoryServiceMock
+    when(restaurantRepositoryServiceMock
             .findAllRestaurantsCloseBy(any(Double.class), any(Double.class),
                 eq(LocalTime.of(3, 0)),
                 eq(5.0)))
